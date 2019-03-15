7d31912e262544e976a692637891fec04c95477f
seth <seth@cmu.edu>
2019-03-15 09:55:40 -0400

    initial add for a local anchor node

---
+++ addons/localanchor.js
@@ -0,0 +1,38 @@
+const C_LEFT_BRACE = 123;
+
+const reAnchor = /^\{\#([A-Za-z0-9_-]+)\}/;
+
+// attempt to parse an anchor
+var parseAnchor = function(block) {
+    const anchor = this.match(reAnchor);
+    if (anchor === null) return false;
+    const match = anchor.match(reAnchor);
+    // we have a match for an anchor
+    let node = new Node('anchor');
+    node.anchorName = match[1];
+    block.appendChild(node);
+    return true;
+}
+
+function renderHTML(node, entering) {
+    if (entering) {
+        this.tag('span', [['id', node.anchorName]], true);
+    }
+}
+
+function renderXML(node, attrs) {
+    attrs.push( ['name', node.anchorName] );
+}
+
+const install = function(aip, html, xml) {
+    Node = aip(C_LEFT_BRACE, 'anchor', parseAnchor, [ ['level', 'litr'] ]);
+    if (html) {
+        html.anchor = renderHTML; 
+    }
+    if (xml) {
+        xml.addRenderer('anchor', renderXML);
+    }
+
+};
+
+module.exports = install;
