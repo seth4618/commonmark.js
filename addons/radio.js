c215e67e1bdcb3fe6a696cc0299677791ce578cc
seth <seth@cmu.edu>
2019-03-27 22:46:04 -0400

    add {blank} processing now install both blocks and inlines

--- addons/radio.js
+++ addons/radio.js
@@ -138,16 +138,120 @@ function answerHTML(node, entering) {
 function answerXML(node, attrs) {
 }
 
-const install = function(abp, html, xml) {
-    Node = abp(nodetype, blockStart, blockfuncs, true, [], '{');
-    if (Node === false) throw "Cannot "+nodetype+" as a block node";
-    
-    if (html) {html[nodetype] = renderHTML;}
-    if (xml) xml.addRenderer(nodetype, renderXML);
-    if (abp('answer', answerStart, answerFuncs, true, [], '{'))
-        if (Node === false) throw "Cannot answer as a block node";
-    if (html) {html['answer'] = answerHTML;}
-    if (xml) xml.addRenderer('answer', answerXML);
+////////////////////////////////////////////////////////////////
+// blank:x
+
+const reBlank = /^\{blank\s*(?::([0-9]+))*\s*\}/;
+
+const parseBlank =  function(block) {
+    const blank = this.match(reBlank);
+    if (blank === null) {
+        return false;
+    }
+    const match = blank.match(reBlank);
+    const width = match[1];
+    let node = new Node('blank');
+    node.qwidth = width || "6";
+    block.appendChild(node);
+    return true;
 };
 
-module.exports = install;
+
+function blankHTML(node, entering) {
+    if (entering) {
+        this.cr();
+        this.lit('<!-- blank -->');
+        this.lit('<!-- '+node.qwidth+' -->');
+        this.cr();
+    }
+}
+
+function blankXML(node, attrs) {
+}
+
+////////////////////////////////////////////////////////////////
+// solutions
+
+const reSolution = /^\{solution\s*:\s*(begin|end)\s*(?::([^}]+))*\s*\}\s*/;
+
+const solutionFuncs =  {
+    continue: function(parser) {
+        var ln = parser.currentLine;
+        var match;
+        if ((peek(ln, parser.nextNonspace) === C_LEFT_BRACE) &&
+            (match = ln.slice(parser.nextNonspace).match(reSolution)) &&
+            (match[1] == 'end')) {
+            // we are done with the solution block
+            parser.advanceOffset(match[0].length, true); // to end of marker
+            return 1;
+        } else {
+            // keep going
+            return 0;
+        }
+    },
+    finalize: function() { return; },
+    canContain: function(t) { return false; return (t === 'paragraph');  },
+    acceptsLines: true
+};
+
+const solutionStart = function(parser, container) {
+    var match;
+    if (!parser.indented &&
+        (peek(parser.currentLine, parser.nextNonspace) === C_LEFT_BRACE) &&
+        (match = parser.currentLine.slice(parser.nextNonspace).match(reSolution)) &&
+        (match[1] == 'begin')) {
+        // we matched a {solution:begin/end:<num>}, advance parser
+        parser.advanceNextNonspace();
+        parser.advanceOffset(match[0].length, false);
+        parser.closeUnmatchedBlocks();
+        let node = parser.addChild('solution', parser.nextNonspace);
+        node.solkey = match[2];
+        return 1;
+    } else {
+        return 0;
+    }
+};
+
+
+function solutionHTML(node, entering) {
+    if (entering) {
+        this.cr();
+        this.lit('<!-- solution -->');
+        this.lit('<!-- '+node._string_content+' -->');
+        this.cr();
+    }
+}
+
+function solutionXML(node, attrs) {
+}
+
+const installers = [ [nodetype, blockStart, blockfuncs, renderHTML, renderXML],
+                     ['answer', answerStart, answerFuncs, answerHTML, answerXML],
+                     ['solution', solutionStart, solutionFuncs, solutionHTML, solutionXML] ];  
+
+const inliners = [ ['blank', parseBlank, blankHTML, blankXML] ];
+
+
+const installBlocks = function(abp, html, xml) {
+    for (const info of installers) {
+        const n = abp(info[0], info[1], info[2], true, [], '{');
+        if (n === false) throw "Cannot install "+info[0]+" as a block node";
+        if ((Node == undefined)&&(Node == null)) Node = n;
+        if (html) {html[info[0]] = info[3];}
+        if (xml) xml.addRenderer(info[0], info[4]);
+    }
+};
+
+const installInlines = function(aip, html, xml) {
+    for (const info of inliners) {
+        aip(C_LEFT_BRACE, info[0], info[1], false, [ ]);
+        if (html) {
+            html[info[0]] = info[2]; 
+        }
+        if (xml) {
+            xml.addRenderer(info[0], info[3]);
+        }
+    }
+};
+
+module.exports = {blocks: installBlocks, inlines: installInlines};
