12b8b57ef3cc901655059c6c017474b96f16a157
seth <seth@cmu.edu>
2019-04-10 00:19:30 -0400

    fixed issues with last cell, support old methods of table when cols not specified, and better parsing of table spec. still has debugging info in it.

--- addons/table.js
+++ addons/table.js
@@ -4,7 +4,7 @@ let Node = null;
 
 const debugAnswer = false;
 const nodetype = 'table';
-const reToken = /^\{\s*table\s*:\s*(begin|end)\s*(?::\s*([0-9]+)\s*:\s*([^ }]+)\s*)?\}/;
+const reToken = /^\{\s*table\s*:\s*(begin|end)\s*(?::\s*([0-9]*)\s*:\s*([^ }]+)\s*)?\}/;
 const C_LEFT_BRACE = 123;
 
 const peek = function(ln, pos) {
@@ -18,10 +18,11 @@ const peek = function(ln, pos) {
 var addCell = function() {
     // see if we need a new row
     //console.log("AC: %d %d | %s", this.cols, this.counter, this.row ? this.row.tostring() : "(null)");
-    if ((this.row == null)||(this.counter == this.cols)) {
+    if ((this.row == null)||(this.counter == this.cols)||this.needNewRow) {
         this.row = new Node('row', this.lastpos);
         this.rows.push(this.row);
         this.counter = 0;
+        this.needNewRow = false;
     }
     const col = new Node('col', this.lastpos);
     this.row.appendChild(col);
@@ -37,17 +38,35 @@ var addNode2cell = function(node) {
     this.cell.appendChild(node);
 };
 
+var td2string = function() {
+    return [ 'cols:', this.cols, 
+             ' sepr:', this.sepr, 
+             ' cntr:', this.counter, 
+             ' rowid:', this.row?this.row._uid:'(null)',
+             ' colid:', this.cell?this.col._uid:'(null)',
+             ' rows:', this.rows.length,
+             ' NNR:', this.needNewRow ? "YES" : "no" ].join("");
+};
+
+// assumption is string has implied new line at end.
 var addString2cell = function(string) {
+    const trimmed = string.trim();
     const pieces = string.split(this.sepr);
+    let needNewCell = false;
     for (let p of pieces) {
+        if (needNewCell) {
+            this.cell = null;
+            needNewCell = false;
+        }
         p = p.trim();
         let n = null;
         if (p != "") {
             n = new Node('paragraph', this.lastpos);
             n._string_content = p;
         }
+        console.log('Adding para with [%s] to %s', p, this.tostring());
         this.addNode2cell(n);
-        this.cell = null;
+        needNewCell = true;	// before adding text here, make sure to add another cell.
     }
 };
 
@@ -56,44 +75,69 @@ RegExp.escape= function(s) {
 };
 
 var TableData = function(cols, sepr, sp) {
+    //const synthSepr = (cols > 0) ? RegExp.escape(sepr) : [ RegExp.escape(sepr) , "|\n" ].join("");
+    //console.log('New TD: %s', synthSepr);
+    const synthSepr = RegExp.escape(sepr);
     return {
         // object data
         cols: cols,	  // number of columns per row
-        sepr: new RegExp(RegExp.escape(sepr)),	  // column separator
+        sepr: new RegExp(synthSepr),	  // column separator
         rows: [],	  // processed rows
         row: null,	  // current row
         cell: null,	  // current cell
         counter: 0,	  // number of cells in this row
-        lastpos: sp,    // last source position
+        lastpos: sp,      // last source position
+        needNewRow: false,// used when one row per line
         // object methods
         addString2cell: addString2cell,
         addNode2cell: addNode2cell,
-        addCell: addCell
+        addCell: addCell,
+        tostring: td2string
     };
 };
 
 // break the subtree into rows and cols
 const createRowsAndColumns = function(parser, block) {
-    //console.log(block.asstring());
+    console.log(block.asstring());
     const columns = block.columns;
     const sepr = block.separator;
-    let tabledata = new TableData(columns, sepr, block.sourcepos);
+    let tabledata = new TableData(columns, sepr || "|", block.sourcepos);
     const children = block.detachChildren();
     for (let child of children) {
         tabledata.lastpos = child.sourcepos;
         if (child.type == 'paragraph') {
-            tabledata.addString2cell(child._string_content);
+            if (columns == 0) {
+                // break into lines and end each line with a new row
+                const lines = child._string_content.split("\n");
+                for (let line of lines) {
+                    console.log('para ==> [%s]', line);
+                    tabledata.addString2cell(line);
+                    tabledata.cell = null;
+                    tabledata.needNewRow = true;
+                }
+            } else {
+                tabledata.addString2cell(child._string_content);
+            }
         } else {
             // add this node to the current cell
+            console.log('else ==>');
             tabledata.addNode2cell(child);
         }
+    }
+    // if content is empty, then just return
+    const len = tabledata.rows.length;
+    if (len == 0) return;
+    // eliminate last row if it contains only 1 cell which is emptry
+    const lastrow = tabledata.rows[tabledata.rows.length-1];
+    if (lastrow.firstChild && (lastrow.firstChild.firstChild == null) && (lastrow.firstChild.next == null)) {
+        tabledata.rows.pop();
     }
     // now turn array of rows into row nodes and attach to table
     for (let r of tabledata.rows) {
         block.appendChild(r);
     }
-    //console.log('-------------');
-    //console.log(block.asstring());
+    console.log('-------------');
+    console.log(block.asstring());
 };
 
 const blockfuncs =  {
@@ -123,13 +167,27 @@ const blockStart = function(parser) {
         (match = parser.currentLine.slice(parser.nextNonspace).match(reToken)) &&
         (match[1] == 'begin')) {
         // we matched a {table:begin:<cols>:<sepr>}, advance parser
+        if (match.length == 3) {
+            // some wierdness
+            return 0;
+        }
         parser.advanceNextNonspace();
         parser.advanceOffset(match[0].length, false);
         parser.closeUnmatchedBlocks();
         var container = parser.addChild(nodetype, parser.nextNonspace);
-        container.columns = parseInt(match[2]);
-        container.separator = match[3];
-        //console.log('columns=%d, separator=%s', container.columns, container.separator);
+        if (match.length >= 3)
+            container.separator = match[3];
+        console.log('%d %s', match.length, match);
+        if ((match.length == 4)&&((typeof match[2] !== 'undefined') && (match[2].trim().length > 0))) {
+            // this is a table that can contain blocks
+            container.columns = parseInt(match[2]);
+            container.lineisrow = false;
+        } else {
+            // this is a table where each line is a row
+            container.columns = 0;
+            container.lineisrow = true;
+        }
+        //console.log('columns=%d, separator=%s %s', container.columns, container.separator, container.lineisrow?"line per row":"");
         return 1;
     } else {
         return 0;
@@ -139,6 +197,7 @@ const blockStart = function(parser) {
 
 function renderHTML(node, entering) {
     if (entering) {
+        console.log(node.asstring());
         this.tag('table', [], false);
     } else {
         this.tag('/table');
